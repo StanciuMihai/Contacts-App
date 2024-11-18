efe61af3fc01151fc204036e36f53afb66c8bf76
Stanciu Mihai <misu_sergiu@yahoo.com>
2024-11-18 20:24:46 +0200

    Final iteration

--- Contacts.Maui/Views/EditContactPage.xaml.cs
+++ Contacts.Maui/Views/EditContactPage.xaml.cs
@@ -24,39 +24,31 @@ public partial class EditContactPage : ContentPage
 			contact = ContactRepository.GetContactById(int.Parse(value));
 			if (contact != null)
 			{
-				entryName.Text = contact.Name;
-				entryAddress.Text = contact.Address;
-				entryEmail.Text = contact.Email;
-				entryPhone.Text = contact.Phone;
+				contactCtrl.Name = contact.Name;
+                contactCtrl.Address = contact.Address;
+                contactCtrl.Email = contact.Email;
+                contactCtrl.Phone = contact.Phone;
 			}
 		}
 	}
 
     private void btnUpdate_Clicked(object sender, EventArgs e)
     {
-		if(nameValidator.IsNotValid)
-		{
-			DisplayAlert("Error", "Name is required.", "OK");
-			return;
-		}
 
-		if (emailValidator.IsNotValid)
-		{
-			foreach (var error in emailValidator.Errors) {
-				DisplayAlert("Error",error.ToString(),"OK");
-			}
 
-			return;
-		}
-
-		contact.Name = entryName.Text;
-        contact.Address = entryAddress.Text;
-        contact.Phone = entryPhone.Text;
-        contact.Email = entryEmail.Text;
+		contact.Name = contactCtrl.Name;
+        contact.Address = contactCtrl.Address;
+        contact.Phone = contactCtrl.Phone;
+        contact.Email = contactCtrl.Email;
 
 		ContactRepository.UpdateContact(contact.ContactId, contact);
 
         Shell.Current.GoToAsync("..");
 
+    }
+
+    private void contactCtrl_OnError(object sender, string e)
+    {
+		DisplayAlert("Error",e,"OK");
     }
 }
