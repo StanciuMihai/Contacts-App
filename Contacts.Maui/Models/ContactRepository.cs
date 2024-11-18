3c7760657b458e910bf7f01014ec337a987db324
Stanciu Mihai <misu_sergiu@yahoo.com>
2024-11-18 20:32:16 +0200

    added preview pictures

--- Contacts.Maui/Models/ContactRepository.cs
+++ Contacts.Maui/Models/ContactRepository.cs
@@ -14,7 +14,7 @@ namespace Contacts.Maui.Models
             new Contact { ContactId=1, Name="John Doe", Email="JohnDoe@gmail.com" },
             new Contact { ContactId=2, Name="Jane Doe", Email="JaneDoe@gmail.com"},
             new Contact { ContactId=3, Name="Tom Hanks", Email="TomHanks@gmail.com"},
-            new Contact { ContactId=4, Name="Frank Liu", Email="FrakLiu@gmail.com"},
+            new Contact { ContactId=4, Name="Frank Sinatra", Email="FrakSinatra@gmail.com"},
         
         };
 
